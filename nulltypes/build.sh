9da640bc075c272e9c3e38e07787f78bd5276614
yhbman@gmail.com <yhbman@gmail.com>
2021-02-07 14:29:26 +0800

    refactor

--- nulltypes/build.sh
+++ nulltypes/build.sh
@@ -1,47 +1,21 @@
 #!/bin/sh
 
 cd empty
-protoc \
-  --go_out=. \
-  --proto_path=.. \
-  empty.proto
-
-mv ./github.com/weiwolves/protobuf/nulltypes/empty/* .
-rm -rf ./github.com
+./build.sh
 cd -
 
 cd nulldate
-protoc \
-  --go_out=. \
-  --proto_path=.. \
-  nulldate.proto
-mv ./github.com/weiwolves/protobuf/nulltypes/nulldate/* .
-rm -rf ./github.com
+./build.sh
 cd -
 
 cd nullint
-protoc \
-  --go_out=. \
-  --proto_path=.. \
-  nulldate.proto
-mv ./github.com/weiwolves/protobuf/nulltypes/nulldate/* .
-rm -rf ./github.com
+./build.sh
 cd -
 
 cd nullstring
-protoc \
-  --go_out=. \
-  --proto_path=.. \
-  nullstring.proto
-mv ./github.com/weiwolves/protobuf/nulltypes/nullstring/* .
-rm -rf ./github.com
+./build.sh
 cd -
 
 cd timestamp
-protoc \
-  --go_out=. \
-  --proto_path=.. \
-  timestamp.proto
-mv ./github.com/weiwolves/protobuf/nulltypes/timestamp/* .
-rm -rf ./github.com
+./build.sh
 cd -
